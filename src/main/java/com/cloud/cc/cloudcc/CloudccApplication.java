f850a68946e22723ec6d924ffbfc19dd2916935b
santosh <santosh@go2market.in>
2024-09-30 15:19:54 +0530

    first code

---
+++ src/main/java/com/cloud/cc/cloudcc/CloudccApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.cc.cloudcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloud/cc/cloudcc/CloudccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.cc.cloudcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
