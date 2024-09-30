f76c837c4387a0a115ce454392f11848c4135996
nitish <nitish@pratikshat.com>
2024-09-30 15:38:34 +0530

    first by nitish

--- src/main/java/com/cloud/cc/cloudcc/CloudccApplication.java
+++ src/main/java/com/cloud/cc/cloudcc/CloudccApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CloudccApplication {
 
 	public static void main(String[] args) {
+		System.out.println("main branch----");
 		SpringApplication.run(CloudccApplication.class, args);
 	}
 
